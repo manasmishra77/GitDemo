af145203b83d492acd846e7b4a499ee4e07e93fb
Manas Mishra <manas1.mishra@ril.com>
2018-08-02 16:14:37 +0530

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Manas Mishra on 02/08/18.
+//  Copyright © 2018 manas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func doSome() {
+        print("11")
+    }
+
+
+}
+
